d9e418c0da00f52ebd72f84b1f2b7fdf7707fe49
44luvv <lvv44.procreator@gmail.com>
2026-09-14 16:28:23 +0300

    try do auth

--- DropX/urls.py
+++ DropX/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from games.views import register
+from games.views import register, main_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', register, name='registration'),
+    path('main/', main_page, name='main'),
 ]
--- games/views.py
+++ games/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 def register(request):
     if request.method == 'POST':
         nickname = request.POST.get('nickname')
         password = request.POST.get('password')
-        print(f"Пришли данные -> Имя: {nickname} | Пароль: {password}")
+
+        if User.objects.filter(username=nickname).exists():
+            print(f"Ошибка: ник {nickname} уже занят!")
+        else:
+            user = User.objects.create_user(username=nickname, password=password)
+            print(f"Успех! Игрок {user.username} записан в Postgres (ID: {user.id})")
 
     return render(request, 'index.html')
+
+def main_page(request):
+    return render(request, 'pages/main.html')
