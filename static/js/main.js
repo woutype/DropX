125290c68a3a08874fbfa178671d011b434d90f4
44luvv <lvv44.procreator@gmail.com>
2026-09-14 14:29:05 +0300

    add fx to button

--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,14 @@
 const nick = document.getElementById('nick');
 const pass = document.getElementById('pass');
+const submit = document.getElementById('submit');
 
 nick.addEventListener('input', () => {
     if (nick.value.trim().length > 0) {
         pass.classList.remove('pass-hidden');
     } else {
         pass.classList.add('pass-hidden');
+        pass.value = '';
+        submit.classList.add('pass-hidden');
     }
 });
 
@@ -16,5 +19,13 @@ nick.addEventListener('keydown', (event) => {
             pass.classList.remove('pass-hidden');
             pass.focus();
         }
+    }
+});
+
+pass.addEventListener('input', () => {
+    if (pass.value.trim().length > 0) {
+        submit.classList.remove('pass-hidden');
+    } else {
+        submit.classList.add('pass-hidden');
     }
 });
